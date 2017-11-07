949c1010bdf9febffe8a8ae83a8e56ef9747454f
zatziky <zatziky@gmail.com>
2017-11-07 13:25:17 +0100

    ref: rename routes

--- src/controllers/demo.controller.js
+++ src/controllers/demo.controller.js
@@ -12,36 +12,7 @@ class DemosController {
 
     const content = {
       "type": "receipt",
-      "payload": {
-        "order_id": "MUST BE UNIQUE = order_number",
-        "timestamp": "2017-01-19T10:31:00Z",
-        "currency": "CZK",
-        "payment_method": "Payment method details. This can be a custom string. ex: \"Visa 1234\".",
-        "url_order": "http://petersapparel.parseapp.com/order?order_id=123456",
-        "shipping_address": {
-          "name": "Stephane Crozatier",
-          "address_line_1": "Ve smečkách 46",
-          "address_line_2": "Amio s.r.o.",
-          "city": "Praha",
-          "postal_code": "11000",
-          "state": "Prague = State abbreviation or Region/Province (international)",
-          "country_code": "CZ"
-        },
-        "items": [
-          {
-            "title": "Classic White T-Shirt",
-            "text": "100% Soft and Luxurious Cotton",
-            "quantity": 2,
-            "price": 50,
-            "image_url": "https://storage.amio.io/Lovelyloops.jpg"
-          }
-        ],
-        "summary": {
-          "shipping": 4.95,
-          "tax": 6.19,
-          "total": 56.14
-        }
-      }
+      "payload": createReceiptPayload()
     }
 
     await messageService.sendMessage(contact, channel, content)
@@ -49,12 +20,67 @@ class DemosController {
   }
 
   static async sendText(req, res, next) {
+    debug('sendReceipt() - body: ', req.body, ', params: ', req.params)
+    debug(os.hostname())
+    const {channel, contact} = req.body
 
+    const content = {
+      "type": "text",
+      "payload": "Vase objednavka 169735313 je po dobu 4 dní pripavena na centrále Alza.cz Jateční 33a, Praha, Po-Ne 0-24. PIN: 8450"
+    }
+
+    await messageService.sendMessage(contact, channel, content)
+    res.json("ok")
   }
 
-  static async sendQuickReplies(req, res, next) {
+  static async sendButtons(req, res, next) {
+    debug('sendReceipt() - body: ', req.body, ', params: ', req.params)
+    debug(os.hostname())
+    const {channel, contact} = req.body
+
+    const content = {
+      "type": "text",
+      "payload": "Vase objednavka 169735313 je po dobu 4 dní pripavena na centrále Alza.cz Jateční 33a, Praha, Po-Ne 0-24. PIN: 8450"
+    }
+
+    await messageService.sendMessage(contact, channel, content)
+    res.json("ok")
 
   }
+}
+
+function createReceiptPayload() {
+  return {
+    "order_id": "MUST BE UNIQUE = order_number",
+    "timestamp": "2017-01-19T10:31:00Z",
+    "currency": "CZK",
+    "payment_method": "Payment method details. This can be a custom string. ex: \"Visa 1234\".",
+    "url_order": "http://petersapparel.parseapp.com/order?order_id=123456",
+    "shipping_address": {
+      "name": "Stephane Crozatier",
+      "address_line_1": "Ve smečkách 46",
+      "address_line_2": "Amio s.r.o.",
+      "city": "Praha",
+      "postal_code": "11000",
+      "state": "Prague = State abbreviation or Region/Province (international)",
+      "country_code": "CZ"
+    },
+    "items": [
+      {
+        "title": "Classic White T-Shirt",
+        "text": "100% Soft and Luxurious Cotton",
+        "quantity": 2,
+        "price": 50,
+        "image_url": "https://storage.amio.io/Lovelyloops.jpg"
+      }
+    ],
+    "summary": {
+      "shipping": 4.95,
+      "tax": 6.19,
+      "total": 56.14
+    }
+  }
+
 }
 
 module.exports = DemosController
--- src/controllers/router.js
+++ src/controllers/router.js
@@ -4,8 +4,8 @@ const errorHandlerMiddleware = require('../middleware/async-error-handler.middle
 const demosController = require('./demo.controller')
 
 router.post('/demos/receipt', errorHandlerMiddleware(demosController.sendReceipt))
-router.post('/demos/text', errorHandlerMiddleware(demosController.sendText))
-router.post('/demos/quick-replies', errorHandlerMiddleware(demosController.sendQuickReplies))
+router.post('/demos/plain-text', errorHandlerMiddleware(demosController.sendText))
+router.post('/demos/choices', errorHandlerMiddleware(demosController.sendButtons))
 
 
 router.get('/', function(req, res, next) {
